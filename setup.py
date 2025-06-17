75b87ea940a104f02e51a9db698928174813debb
Amogus <beowulf81058105@gmail.com>
2025-06-17 20:39:08 +0200

    fixed typo

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="pyopengl-wrapper", version="0.1.0", py_modules=["pyopengl-Wrapper"])
+setup(name="pyopengl-wrapper", version="0.1.0", py_modules=["pyopenglWrapper"])
